999c8c9aef956e20e0fd83b1c358d3653721f2c1
ssenecal <95314184+ssenecal@users.noreply.github.com>
2025-03-13 16:23:34 +0100

    Updated next.config.ts to embed security measures

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,27 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY", // Blocks all iframes
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'none';", // Blocks framing
+          },          
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=7776000; includeSubDomains; preload",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
